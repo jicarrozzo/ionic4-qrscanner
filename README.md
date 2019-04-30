3304df5e229a4c2f13e414c32961fa7c91e865df
Juan Ignacio Carrozzo <Lobezno@Lobocop.sisorg.com.ar>
2019-04-30 13:54:21 -0300

    first setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# ionic4-qrscanner
+
+QR Scanner App help you save information about your QR codes! 
+
+
+Base on Fernando Herrera's Ionic course from udemy.com
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	entryComponents: [],
+	imports: [ BrowserModule, IonicModule.forRoot(), AppRoutingModule ],
+	providers: [ StatusBar, SplashScreen, BarcodeScanner, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy } ],
+	bootstrap: [ AppComponent ]
 })
 export class AppModule {}
